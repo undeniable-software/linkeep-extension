e2e508d932eca52ccae1ad79f6dd5baf23965ebf
roy. <roywashington1547@gmail.com>
2024-07-21 13:29:15 -0500

    config changes

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from 'wxt';
 import react from '@vitejs/plugin-react';
 
+const apiUrl = import.meta.env.LINKEEP_API_URL;
+
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: 'src',
   entrypointsDir: 'entries',
   manifest: {
-    permissions: ['activeTab', 'webRequest'],
+    permissions: ['activeTab', 'webRequest', 'cookies', 'storage'],
+    host_permissions: [apiUrl + '/*'],
+    web_accessible_resources: [
+      {
+        matches: ['<all_urls>'],
+        resources: ['popup.html'],
+      },
+    ],
   },
   vite: () => ({
     plugins: [react()],
